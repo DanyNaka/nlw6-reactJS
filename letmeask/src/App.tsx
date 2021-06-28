85c1237ffeca905fb8d72cc652625515141d4426
DanyNaka <daninakassima@gmail.com>
2021-06-28 16:45:45 -0300

    Aula 02 - até 40:50

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,14 +1,8 @@
-import {Button} from './componentes/Button'
+import {Home} from './pages/Home'
 
 function App() {
   return (
-    <div>
-      <Button text="Botão 1" />
-      <Button />
-      <Button />
-      <Button />
-      <Button></Button>
-    </div>
+    <Home />
   );
 }
 
